f6e0ac5468e6413a7b440fe71fc509c5c8ef587f
Flosi89 <florian.wiss2@bluewin.ch>
2025-10-01 20:55:56 +0300

    Delete functions/api/[id].js

--- functions/api/[id].js
+++
@@ -1,9 +0,0 @@
-export async function onRequestDelete({ params, env }) {
-  await env.DB.prepare("DELETE FROM customers WHERE id = ?")
-    .bind(params.id)
-    .run();
-
-  return new Response(JSON.stringify({ success: true }), {
-    headers: { "Content-Type": "application/json" },
-  });
-}
